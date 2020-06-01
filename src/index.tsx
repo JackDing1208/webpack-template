bd9c000fff6add609e3cbb7ac38346e00fcedd85
jack <361164217@qq.com>
2020-06-01 10:30:02 +0800

    ts配好了

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = ()=>{
-  return(
+const App: React.FC = () => {
+  console.log(process.env)
+  return (
     <div>haha</div>
   )
 }
 
 
-ReactDOM.render(<App/>,document.getElementById("root"))
+ReactDOM.render(<App/>, document.getElementById("root"))
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,50 @@
-const path = require('path');
-
+const path = require("path")
+const HtmlWebpackPlugin = require("html-webpack-plugin")
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const webpack = require("webpack")
 module.exports = {
-  mode: 'development',
+  mode: "development",
   // 入口 这里应用程序开始执行
-  entry: path.resolve(__dirname, 'src/index.tsx'),
-  // 出口
+  entry: path.resolve(__dirname, "src/index.tsx"),
+  // 打包出口
   output: {
     // 输出文件的目标路径
-    path: path.resolve(__dirname, 'dist'),
-    // 输出的文件名
-    filename: 'bundle.js',
+    path: path.resolve(__dirname, "dist"),
+    // 输出的文件名  默认为main.js
+    filename: 'index.js',
     // 输出解析文件的目录。静态资源最终访问路径 = output.publicPath + 资源loader或插件等配置路径
-    publicPath: '/'
-  }
+    publicPath: "/",
+  },
+  // dev端口
+  devServer: {
+    hot: true,
+    port: 3000,
+    open: true,
+    contentBase: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    // stats: 'none'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(tsx|ts)?$/,
+        loader: "awesome-typescript-loader",
+        exclude: /node_modules/,
+        include: path.resolve(__dirname, "src"),
+      },
+    ],
+  },
+  plugins: [
+    //自动关联HTML用的插件
+    new HtmlWebpackPlugin({
+      template: "index.html",
+      title: "webpack-template",
+    }),
+    //注入全局环境变量
+    new webpack.DefinePlugin({
+      PRODUCTION: JSON.stringify(true),
+      "process.env": JSON.stringify(process.env.NODE_ENV),
+    }),
+    new CleanWebpackPlugin()
+  ],
 }
